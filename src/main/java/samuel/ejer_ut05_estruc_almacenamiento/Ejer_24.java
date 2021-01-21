50547ac769f49d744e636171927cb0d933c5c0b4
samuel <samubp10@gmail.com>
2021-01-21 13:54:14 +0100

    Crea el proyecto de ejercicio 24

---
+++ src/main/java/samuel/ejer_ut05_estruc_almacenamiento/Ejer_24.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samuel.ejer_ut05_estruc_almacenamiento;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author samuel
+ */
+public class Ejer_24 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> lista1 = new ArrayList<>();
+        ArrayList<Integer> lista2 = new ArrayList<>();
+        Random random = new Random();
+        int enteroAleatorio01;
+        int enteroAleatorio02;
+        int enteroAleatorio03 = random.nextInt(15 - 5 + 1) + 5;
+        enteroAleatorio02 = random.nextInt(20 - 10 + 1) + 10;
+        for (int i = 0; i <= enteroAleatorio03; i++) {
+            enteroAleatorio01 = random.nextInt(100 - 50 + 1) + 50;
+            lista1.add(enteroAleatorio01);
+        }
+        for (int i = 0; i <= enteroAleatorio02; i++) {
+            enteroAleatorio01 = random.nextInt(100 - 50 + 1) + 50;
+            lista2.add(enteroAleatorio01);
+        }
+        System.out.println("Lista 1: ");
+        for (Integer tmp1 : lista1) {
+            System.out.println(tmp1);
+
+        }
+        System.out.println("Lista 2: ");
+        for (Integer tmp2 : lista2) {
+            System.out.println(tmp2);
+
+        }
+        ArrayList<Integer> lista3 = new ArrayList<>();
+
+        for (int tmp : lista1) {
+            lista3.add(tmp);
+        }
+        for (int tmp : lista2) {
+            for (int tmp2 : lista1) {
+                if (tmp == tmp2) {
+                    lista3.remove(tmp2);
+                }
+            }
+
+        }
+
+        System.out.println("Lista 3: ");
+        for (Integer tmp3 : lista3) {
+            System.out.println(tmp3);
+
+        }
+        ArrayList<Integer> lista4 = new ArrayList<>();
+
+        for (Integer tmp4 : lista1) {
+            if (tmp4 % 2 == 0) {
+                lista4.add(tmp4);
+            }
+        }
+        for (Integer tmp5 : lista2) {
+            if (tmp5 % 2 != 0) {
+                lista4.add(tmp5);
+            }
+        }
+        System.out.println("Lista 4: ");
+        lista4.forEach(System.out::println);
+    }
+}

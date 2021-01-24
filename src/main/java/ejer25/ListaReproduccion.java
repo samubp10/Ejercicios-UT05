598cf97bdad2ec5b9b5ed1f8d736351813effa62
samuel <samubp10@gmail.com>
2021-01-25 00:47:13 +0100

    Crea el ejercicio 25

---
+++ src/main/java/ejer25/ListaReproduccion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer25;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ListaReproduccion {
+    Cancion canciones;
+
+    public ListaReproduccion() {
+    }
+    
+    public int numeroCanciones(){
+        
+        return;
+    }
+    
+    public boolean estaVacia(){
+        return;
+    }
+    
+    public int escucharCancion(){
+        return;
+    }
+    
+    public int cambiarCancion(int Cancion){
+        return;
+    }
+    
+    public void grabarCancion(Cancion){
+        
+    }
+    
+    public void eliminaCancion(int h){
+        
+    }
+    
+    public static imprimirLista(ListaReproduccion tmp){
+        
+        return;
+    }
+    
+    public int buscarCancion(Cancion c){
+        
+    }
+    
+    
+}
